eef7781f66f5fca5d06ef77e73873116b664b047
Enes <152513477+enessonmez-ceng@users.noreply.github.com>
2025-10-26 18:14:43 +0300

    Update main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -3,9 +3,26 @@ import numpy as np
 import statistics
 
 
-
-
 df = pd.read_csv("Data/titanic/train.csv")
+
+def handle_outliers_iqr(df, column):
+    """
+    Bir sütundaki aykırı değerleri IQR yöntemine göre baskılar (capping).
+    """
+    Q1 = df[column].quantile(0.25)
+    Q3 = df[column].quantile(0.75)
+    IQR = Q3 - Q1
+
+    
+    lower_bound = Q1 - 1.5 * IQR
+    upper_bound = Q3 + 1.5 * IQR
+
+    print(f"'{column}' için Aykırı Değer Sınırları: Alt={lower_bound:.2f}, Üst={upper_bound:.2f}")
+
+  
+    df[column] = np.clip(df[column], lower_bound, upper_bound)
+    return df
+
 
 def complete_missing_values(df):
 
@@ -31,11 +48,16 @@ def prepare_data(df):
 
     df["Embarked"] = df["Embarked"].map(embarked_map)
 
-    df.drop(["Name","Ticket", "Cabin","PassengerId","Sex","SibSp","Parch"], axis=1, inplace=True)
+    df.drop(["Name","Ticket", "Cabin","PassengerId","Sex","SibSp","Parch","IsAlone"], axis=1, inplace=True)
 
     return df
 
+
 df = complete_missing_values(df)
+
+df = handle_outliers_iqr(df, 'Fare')
+df = handle_outliers_iqr(df, 'Age')
+
 data = prepare_data(df)
 
 def get_data():
